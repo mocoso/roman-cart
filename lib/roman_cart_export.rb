550d683a2e3618ae20cfb863638efda49adc77ee
Joel Chippindale <joel@joelchippindale.com>
2014-03-10 05:34:55 +0000

    Extract RomanCartExport

---
+++ lib/roman_cart_export.rb
@@ -0,0 +1,75 @@
+require 'array'
+require 'csv'
+require 'zip/zipfilesystem'
+
+class RomanCartExport
+  def initialize(zip_file)
+    parse_download_data zip_file
+  end
+
+  def csv
+    sales_csv || raise("unable to identify sales file")
+    extra_csv || raise("unable to identify extra data file")
+
+    CSV.generate do |csv|
+      items_csv.each do |row|
+        row = row.dup
+        sales_id = row.shift.strip
+        puts "processing #{sales_id} for #{row[1]}"
+        row.pad(items_csv[0].size - 1, '')
+        csv << sales_output_row(sales_csv, sales_id) + row + extra_data_output_row(extra_csv, sales_id)
+      end
+    end
+  end
+
+  private
+  attr_accessor :items_csv, :sales_csv, :extra_csv
+
+  def parse_download_data(zip_file)
+    Zip::ZipFile.open(zip_file) do |files|
+      files.each do |file|
+        if file.name.match(/\.csv$/)
+          csv = CSV.parse(file.get_input_stream)
+          case csv[0][1].strip
+          when 'companyname'
+            self.sales_csv = csv
+          when 'Extrafieldname'
+            self.extra_csv = csv
+          when 'itemcode'
+            self.items_csv = csv
+          else
+            raise "Unrecognised CSV file"
+          end
+        end
+      end
+    end
+  end
+
+  def sales_id_is_header_row?(sales_id)
+    sales_id == 'salesmainid'
+  end
+
+  def sales_output_row(sales_input, sales_id)
+    if sales_id_is_header_row?(sales_id)
+      sales_input[0]
+    else
+      sales_input_row = sales_input.detect { |r| r[0].strip == sales_id } || raise("Missing sale information for sale with id #{sales_id}")
+      output = sales_input_row.dup
+      output.pad(sales_input[0].size, '')
+      output
+    end
+  end
+
+  def extra_data_output_row(extra_data_input, sales_id)
+    if sales_id_is_header_row?(sales_id)
+      ['Where did you first hear about Slug Rings?']
+    else
+      if extra_data_row = extra_data_input.detect { |r| r[0].strip == sales_id }
+        extra_data_row.slice(2, 1)
+      else
+        ['']
+      end
+    end
+  end
+end
+
--- lib/roman_cart_site.rb
+++ lib/roman_cart_site.rb
@@ -1,8 +1,6 @@
 require 'tmpdir'
-require 'csv'
 require 'mechanize'
-require 'array'
-require 'zip/zipfilesystem'
+require 'roman_cart_export'
 
 class RomanCartSite
   def initialize
@@ -50,8 +48,8 @@ class RomanCartSite
     zip_file.save_as tmp_zip_file
 
     puts "Combining data files"
-    downloaded_data = parse_download_data(tmp_zip_file)
-    generate_combined_data_csv(data_file_path, downloaded_data)
+    csv = RomanCartExport.new(tmp_zip_file).csv
+    File.open(data_file_path, 'w') { |file| file.write(csv.to_s) }
 
     puts "Delete #{tmp_zip_file}"
     system "rm #{tmp_zip_file}"
@@ -63,70 +61,5 @@ class RomanCartSite
   attr_accessor :session_query_string
 
   attr_accessor :agent
-
-  def parse_download_data(zip_file)
-    downloaded_data = {}
-    Zip::ZipFile.open(zip_file) do |files|
-      files.each do |file|
-        if file.name.match(/\.csv$/)
-          csv = CSV.parse(file.get_input_stream)
-          case csv[0][1].strip
-          when 'companyname'
-            downloaded_data['sales'] = csv
-          when 'Extrafieldname'
-            downloaded_data['extra_data'] = csv
-          when 'itemcode'
-            downloaded_data['items'] = csv
-          else
-            raise "Unrecognised CSV file"
-          end
-        end
-      end
-    end
-    downloaded_data
-  end
-
-  def generate_combined_data_csv(data_file_path, downloaded_data)
-    items_input = downloaded_data['items']
-    sales_input = downloaded_data['sales'] || raise("unable to identify sales file")
-    extra_data_input = downloaded_data['extra_data'] ||  raise("unable to identify extra data file")
-
-    CSV.open(data_file_path, "w") do |csv|
-      items_input.each do |row|
-        row = row.dup
-        sales_id = row.shift.strip
-        puts "processing #{sales_id} for #{row[1]}"
-        row.pad(items_input[0].size - 1, '')
-        csv << sales_output_row(sales_input, sales_id) + row + extra_data_output_row(extra_data_input, sales_id)
-      end
-    end
-  end
-
-  def sales_id_is_header_row?(sales_id)
-    sales_id == 'salesmainid'
-  end
-
-  def sales_output_row(sales_input, sales_id)
-    if sales_id_is_header_row?(sales_id)
-      sales_input[0]
-    else
-      sales_input_row = sales_input.detect { |r| r[0].strip == sales_id } || raise("Missing sale information for sale with id #{sales_id}")
-      output = sales_input_row.dup
-      output.pad(sales_input[0].size, '')
-      output
-    end
-  end
-
-  def extra_data_output_row(extra_data_input, sales_id)
-    if sales_id_is_header_row?(sales_id)
-      ['Where did you first hear about Slug Rings?']
-    else
-      if extra_data_row = extra_data_input.detect { |r| r[0].strip == sales_id }
-        extra_data_row.slice(2, 1)
-      else
-        ['']
-      end
-    end
-  end
 end
 
